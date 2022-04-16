5d1e8c4c7db4382b25a90895819a1ad2819715c1
mariave <mariave1987@gmail.com>
2022-04-16 10:09:53 +0200

    added handlekurv on website

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 use App\Repository\ProduktRepository;
@@ -21,6 +23,33 @@ class ProductsController extends AbstractController
             'bikes' => $bikes
         ]);
 
+    }
+
+    /**
+     * @Route("/produkt/{id}")
+     */
+
+    public function details($id, Request $request, ProduktRepository $repo, SessionInterface $session): Response
+    {
+        $bike = $repo->find($id);
+
+        if ($bike === null){
+            throw $this->createNotFoundException('Beklager, produktet eksisterer ikke lenger');
+        }
+
+        //lagt inn til kurven handling
+        $basket = $session->get('basket', []);
+
+        if ($request->isMethod('POST')) {
+            $basket[$bike->getId()] = $bike;
+            $session->set('basket', $basket);
+        }
+        $isInBasket = array_key_exists($bike->getId(),$basket);
+
+        return $this->render('details.html.twig', [
+            'bike' => $bike,
+            'inBasket' => $isInBasket
+        ]);
     }
 
 }
